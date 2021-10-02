3f679ac19b404a5073337a44ba7b169d46335bd3
monowebjp <capybara@monoweb.jp>
2021-10-02 20:22:37 +0900

    initial commit

--- src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    println("What is your name?")
-
-    val name = readLine()
-    println("Hello, $name")
-
-    // Try adding program arguments at Run/Debug configuration
-//    println("Program arguments: ${args.joinToString()}")
-}
