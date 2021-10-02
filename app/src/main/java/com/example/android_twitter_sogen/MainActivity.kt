ab507adffaf5048957276d966ba45fc3f0a71c35
monowebjp <capybara@monoweb.jp>
2021-10-02 23:18:30 +0900

    tweetだけならできる

--- app/src/main/java/com/example/android_twitter_sogen/MainActivity.kt
+++ app/src/main/java/com/example/android_twitter_sogen/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android_twitter_sogen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import kotlinx.coroutines.*
 import twitter4j.TwitterFactory
@@ -28,14 +29,17 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private fun onClick() {
         launch {
+//            ツイートを入力する
+            val editTweet = findViewById<EditText>(R.id.editTweet)
             val textview = findViewById<TextView>(R.id.textView)
             textview.text = "Now Sending"            //ここはメインスレッドで動作するのでViewの変更ができる
 
             async(context = Dispatchers.IO) {
                 val twitter = TwitterFactory().getInstance()
-                twitter.updateStatus("草原の工房からツイートするテスト〜")      //ツイートの投稿
+//                twitter.updateStatus(editTweet.text.toString())      //ツイートの投稿
             }.await()                                //.await()で通信処理が終わるまで待機
 
+            editTweet.text = null
             textview.text = "finish"
         }
     }
