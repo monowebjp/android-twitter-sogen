395784cb1174fb10429eed588da5497de7e533b7
monowebjp <capybara@monoweb.jp>
2021-10-02 22:22:35 +0900

    initial commit

--- app/src/main/java/com/example/android_twitter_sogen/MainActivity.kt
+++ app/src/main/java/com/example/android_twitter_sogen/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.android_twitter_sogen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.*
+import twitter4j.TwitterFactory
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+//プロパティファイルを使う場合
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    //Coroutinesを扱うための設定（詳細は後述）
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {                  //ID:buttonのボタンをクリックした際の処理
+            onClick()
+        }
+    }
+
+    private fun onClick() {
+        launch {
+            val textview = findViewById<TextView>(R.id.textView)
+            textview.text = "Now Sending"            //ここはメインスレッドで動作するのでViewの変更ができる
+
+            async(context = Dispatchers.IO) {
+                val twitter = TwitterFactory().getInstance()
+                twitter.updateStatus("草原の工房からツイートするテスト〜")      //ツイートの投稿
+            }.await()                                //.await()で通信処理が終わるまで待機
+
+            textview.text = "finish"
+        }
+    }
+
+    override fun onDestroy() {
+        job.cancel()                                 //すべてのコルーチンキャンセル用
+        super.onDestroy()
     }
 }
